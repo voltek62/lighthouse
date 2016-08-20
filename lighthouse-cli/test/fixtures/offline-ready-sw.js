2c8e0b75aa127177c2d0b98e2ec43b87a9ae3a07
Paul Irish <paul.irish@gmail.com>
2016-08-19 19:42:53 -0700

    license and cleanup.

--- lighthouse-cli/test/fixtures/offline-ready-sw.js
+++ lighthouse-cli/test/fixtures/offline-ready-sw.js
@@ -61,22 +61,22 @@ self.addEventListener('activate', event => {
 // from the network before returning it to the page.
 self.addEventListener('fetch', event => {
   // Skip cross-origin requests, like those for Google Analytics.
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
-            // Put a copy of the response in the runtime cache.
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
-      })
-    );
+  if (!event.request.url.startsWith(self.location.origin)) {
+    return;
   }
+
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return caches.open(RUNTIME).then(cache => {
+        return fetch(event.request).then(response => {
+          // Put a copy of the response in the runtime cache.
+          return cache.put(event.request, response.clone()).then(_ => response);
+        });
+      });
+    })
+  );
 });
